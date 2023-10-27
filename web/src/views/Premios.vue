562defe2c45cae514ced0b664c10d6111dcf8c4a
LucioCapezzuto <luciodimascapezzuto@gmail.com>
2023-10-27 09:20:57 -0300

    Ultimo commit para mostrar

--- web/src/views/Premios.vue
+++ web/src/views/Premios.vue
@@ -1,6 +1,22 @@
 <template>
     <q-page>
-        
+        <main>
+            <section>
+                <div v-for="(elemento, index) in  cards" :key="index" class="contenedor-premios">
+                    <div class="row">
+                        <div class="col-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h3 class="card-title">Title</h3>
+                                    <p class="card-text">Text</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+        </main>
     </q-page>
 </template>
 
@@ -10,9 +26,29 @@ export default {
     components: {
 
     },
+    data() {
+        return {
+            cards: [
+                {
+                    url: '',
+                    alt: '',
+                    descripcion: '',
+                }
+            ]
+        }
+    },
     mounted() {
         document.title = 'Premios';
     }
 }
 
 </script>
+
+<style>
+.contenedor-premios {
+    display: grid;
+    grid-template-columns: repeat(auto-fit,
+            minmax(200px, 1fr));
+    gap: 1;
+}
+</style>
