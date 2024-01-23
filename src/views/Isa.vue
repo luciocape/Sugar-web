04877725951e78f1885d896cd39e32262d67fcb6
Rama <ramirocarnicersouble8@gmail.com>
2024-01-23 12:42:04 -0300

    Pitch Deck link added

--- src/views/Isa.vue
+++ src/views/Isa.vue
@@ -2,7 +2,7 @@
   <q-page>
       <div style="width: 500px;height: 500px; margin-top: 20vh; display: flex; justify-content: center; align-items: center;" >
          <img src="../../public/contenido/Linktree_Isa.png" style="margin-left: 50%; width: 500px; object-fit: contain;max-width: 100%; max-height: 100%; display: block;" />
-         <a href="https://google.com" target="_blank" style="position: absolute; margin-top: -75px; margin-left: 250px; width: 170px; height: 23px;"></a>
+         <a href="https://www.linkedin.com/feed/update/urn:li:activity:7095142348136865793" target="_blank" style="position: absolute; margin-top: -75px; margin-left: 250px; width: 170px; height: 23px;"></a>
          <a href="https://www.sugar.coach" target="_blank" style="position: absolute; margin-top: 10px; margin-left: 250px; width: 170px; height: 23px;"></a>
          <a href="https://www.linkedin.com/company/sugar-coach/" target="_blank" style="position: absolute; margin-top: 95px; margin-left: 250px; width: 170px; height: 23px;"></a>
          <a href="https://google.com" target="_blank" style="position: absolute; margin-top: 180px; margin-left: 250px; width: 170px; height: 23px;"></a>
